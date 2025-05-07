bed85c14d6bb655b707f9657422b10ebdf9b16b0
ach wahyu arsyafdi <achwahyuarsyafdi@gmail.com>
2025-05-08 01:02:40 +0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function () {
+  var typed = new Typed(".multiple-text", {
+    strings: ["PHOTOGRAFER", "UI/UX Design", "VIDEOGRAVER"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true,
+  });
+});
